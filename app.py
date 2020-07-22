3f86d950fd393f17761de11ae1adbba3451dae8b
TSGreen <tim.green2011@gmail.com>
2020-07-22 21:04:41 +0600

    add dependencies

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 # Build App
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'}
